b507bca61990ecd4ab3e3755b31d4a91e1a04ee7
Ghaining <821741537@qq.com>
2017-05-08 18:36:01 +0800

    no-webpack

--- index.js
+++ index.js
@@ -2,8 +2,37 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.use('/', express.static('views'));
+app.use('/', express.static('bower_components'));
+
 app.get('', function(req, res) {
-	res.send('hello, world!');
+	res.redirect('/index.html')
+	// res.send('hello, world!');
+})
+
+app.post('/getRes', function(req, res) {
+	var n = Math.random();
+	var json = {
+		level: 4,
+		msg: '谢谢'
+	};
+	if(n < 0.1){
+		json.level = 1,
+		json.msg = '1等奖'
+	};
+	if(n < 0.3){
+		json.level = 2,
+		json.msg = '2等奖'
+	}
+	if(n < 0.6){
+		json.level = 3,
+		json.msg = '3等奖'
+	}
+
+	res.json({
+		code: 200,
+		data: json
+	})
 })
 
 app.listen(port, function(req, res) {
